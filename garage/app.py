898f2e2e3c15db81c51d79fcf50e9a83706f58e9
devin stevenson <emberdesign@gmail.com>
2018-11-24 21:44:52 -0800

    add LED pin config

--- garage/app.py
+++ garage/app.py
@@ -2,13 +2,15 @@ import RPi.GPIO as gpio
 from flask import Flask, render_template
 
 REED_PIN = 12
+LED = 16
 
 gpio.setmode(gpio.BCM)
 gpio.setup(REED_PIN, gpio.IN)
+gpio.setup(LED, gpio.OUT)
 
 app = Flask(__name__)
 
-SWITCH_TYPE = 'NO'
+SWITCH_TYPE = 'NO'  # normally open, change to NC for normally closed
 
 
 @app.route('/')
@@ -18,7 +20,16 @@ def index():
 
 
 def get_pin_status():
+    """Read status of the reed switch pin"""
     return gpio.input(REED_PIN) ^ (SWITCH_TYPE == 'NO')
+
+
+def set_led(high=True):
+    """Turn on/off led pin"""
+    if high:
+        gpio.output(LED, gpio.HIGH)
+    else:
+        gpio.output(LED, gpio.LOW)
 
 
 if __name__ == "__main__":
